3823121f70bf0f173ffca3d909fb2605da36a772
Bruno Ramos <62392117+brunoramps@users.noreply.github.com>
2024-04-24 01:49:22 -0300

    Fix to close browser

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 import puppeteer from 'puppeteer';
 
-let abas = 1;
-let nAba = 1;
+let quantidadeDeAbasSimultaneas = 5;
 
-const browser = await puppeteer.launch({
-    headless: true        
+let browser = await puppeteer.launch({
+    headless: false        
 });
 const userAgent = 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';    
 const url = 'https://www.pingoo.casa/torneira-para-banheiro-cascata-misturador-monocomando-alta-parana-prata';
 
-while(abas>0){
+let contadorAbaAtual = 1;
+while(quantidadeDeAbasSimultaneas>0){
 
     const page = await browser.newPage();
     await page.setUserAgent(userAgent);
-    testarHome(page,browser, nAba);
+    testarHome(page,browser, contadorAbaAtual);
     
-    console.log(`aba ${nAba} inciada`);
+    console.log(`aba ${contadorAbaAtual} inciada`);
   
-    abas--;
-    nAba++;
+    quantidadeDeAbasSimultaneas--;
+    contadorAbaAtual++;
 }
 
-async function testarHome(page,browser, nAba){    
+async function testarHome(page,browser, contadorAbaAtual){    
     
-    const numAba = nAba;
+    const numAba = contadorAbaAtual;
     const response = await page.goto(url, {waitUntil: 'networkidle2', timeout: 0});
     
     const title = await page.title();
@@ -37,6 +37,11 @@ async function testarHome(page,browser, nAba){
     }
     
     await page.close();
+    (await browser.pages()).pop()    
     console.log(`Aba ${numAba}: ${status} | ${title}`)
-    if(numAba==abas+1) await browser.close();
+    
+    if((await browser.pages()).length==1) {
+        await browser.close();
+        console.log("Teste finalizado.")
+    };
 }
